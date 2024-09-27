67a90f547cfa8464bc9c06a60a7edc24d89baa11
HerbTeaCup <132678572+HerbTeaCup@users.noreply.github.com>
2024-09-27 17:16:29 +0900

    UIManager 살짝 수정

--- Assets/Scripts/Manangers/UIManager.cs
+++ Assets/Scripts/Manangers/UIManager.cs
@@ -34,7 +34,7 @@ public class UIManager
         //활성화 상태면 창을 닫고, 비활성 상태면 창을 엶.
         if (PopWindow[key].activeSelf)
         {
-            Close();
+            Close(key);
         }
         else
         {
@@ -60,11 +60,6 @@ public class UIManager
             Debug.LogError("UI 오브젝트 아님");
             return;
         }
-        if (window.activeSelf == true)
-        {
-            Debug.Log("이미 활성화되어 있음");
-            return;
-        }
 
         //오브젝트 활성화하고 창을 제일 상단에 노출
         window.SetActive(true);
@@ -73,10 +68,39 @@ public class UIManager
         //창을 닫는 것도 필요하므로 stack에 Push
         UiStack.Push(window);
     }
-    
-    void Close()
+    void Close(string key)
     {
         //창을 닫는 메소드
+        //key 값에 해당하는 오브젝트만 닫음
+
+        //값이 없으면 리턴
+        if (PopWindow.ContainsKey(key) == false)
+        {
+            return;
+        }
+
+        GameObject window = PopWindow[key];
+        UIObject ui;
+
+        //예외처리
+        if (window.TryGetComponent<UIObject>(out ui) == false)
+        {
+            Debug.LogError("UI 오브젝트 아님");
+            return;
+        }
+
+        //오브젝트 비활성화하고 창을 제일 하단에 노출
+        window.SetActive(false);
+        ui.Push();
+
+        //창을 닫는 것도 필요하므로 stack에 Push
+        UiStack.Push(window);
+    }
+
+    void StackClose()
+    {
+        //창을 닫는 메소드
+        //etc. ESC버튼으로 열린 창들 다 닫기
 
         //아무것도 없는데 창을 닫으려고 하면 그냥 리턴
         if(UiStack.Count == 0) { return; }
